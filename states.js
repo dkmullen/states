57127bc1a662a9f5d3b569bd0073dd958efd1801
Dennis Mullen <dkmullen@gmail.com>
2021-03-19 13:05:23 -0400

    states

---
+++ states.js
@@ -0,0 +1,24 @@
+const states = {
+    states_abbr: [
+        'TN', 'KY', 'AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA',
+        'GU','HI','ID','IL','IN','IA','KS','LA','ME','MH','MD','MA',
+        'MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND',
+        'MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TX','UT',
+        'VT','VI','VA','WA','WV','WI','WY'
+    ],
+    states:
+    [
+        'TN - Tennessee', 'KY - Kentucky', 'GA - Georgia', 'NC - North Carolina', 'VA - Virginia',
+        'AK - Alaska', 'AL - Alabama', 'AR - Arkansas', 'AZ - Arizona', 'CA - California', 'CO - Colorado',
+        'CT - Connecticut', 'DE - Delaware', 'FL - Florida', 'HI - Hawaii', 'IA - Iowa', 'ID - Idaho',
+        'IL - Illinois', 'IN - Indiana', 'KS - Kansas', 'LA - Louisiana', 'MA - Massachusetts',
+        'MD - Maryland', 'ME - Maine', 'MI - Michigan', 'MN - Minnesota', 'MO - Missouri',
+        'MS - Mississippi', 'MT - Montana', 'NE - Nebraska', 'ND - North Dakota', 'NH - New Hampshire',
+        'NJ - New Jersey', 'NM - New Mexico', 'NV - Nevada', 'NY - New York', 'OH - Ohio',
+        'OK - Oklahoma', 'OR - Oregon', 'PA - Pennsylvania', 'RI - Rhode Island', 'SC - South Carolina',
+        'SD - South Dakota', 'TX - Texas', 'UT - Utah', 'VT - Vermont', 'WA - Washington', 'WI - Wisconsin',
+        'WV - West Virginia', 'WY - Wyoming', 'AS - American Samoa', 'DC - District of Columbia',
+        'FM - Federated States of Micronesia', 'GU - Guam', 'MH - Marshall Islands',
+        'MP - Northern Mariana Islands', 'PW - Palau', 'PR - Puerto Rico', 'VI - Puerto Rico'
+    ]
+}
